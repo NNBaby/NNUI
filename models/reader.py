c57f975dea754313d84380cb038a5ad8a1686321
wkcn <wkcn@live.cn>
2017-11-21 20:03:23 +0800

    normalize for mnist input

--- models/reader.py
+++ models/reader.py
@@ -113,8 +113,8 @@ class Model:
         rows, cols = 28, 28
         num_classes = 10
         # NHWC
-        x_train = x_train.reshape((x_train.shape[0], rows, cols, 1))
-        x_test = x_test.reshape((x_test.shape[0], rows, cols, 1))
+        x_train = x_train.reshape((x_train.shape[0], rows, cols, 1)) / 255.0
+        x_test = x_test.reshape((x_test.shape[0], rows, cols, 1)) / 255.0
         y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
         y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
         self.model.fit(x = {"data": x_train}, y = {"pred": y_train}, batch_size = 128, epochs = 5)
