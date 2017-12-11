4677e298ed47eb9619ed2b3041bc59d4230ccb8f
Kan Wu (MSR Student-FA Talent) <v-wukan@microsoft.com>
2017-12-11 13:21:12 +0800

    add executor and multiprocessing

--- server/executor/__init__.py
+++ server/executor/__init__.py
@@ -1,1 +1,1 @@
-from .reader import *
+from .executor import *
--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import json
-from executor import Model
+from executor import new_model
 app = Flask(__name__)
 
 @app.route('/get/')
@@ -16,15 +16,17 @@ def get_res():
     jsonpack = request.json
     print('This is json message :', jsonpack,'\n\n')
     return jsonify(jsonpack)
-
+    
 @app.route('/post', methods = ['POST'])
 def post_res():
     jsonpack = request.json
     # the type of jsonpack is dict
     print('This is json message :', jsonpack, '\n\n')
-    model = executor.Model(mode = "train")
-    model.read_json(jsonpack)
-    return jsonify(jsonpack)
+    mid = new_model(jsonpack)
+    result = {
+        "id": mid
+    }
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, use_debugger=True)
